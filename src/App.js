eb32e8ec67e3e32126e0bbf350a8ebccd66de88e
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-09-20 16:58:45 +0200

    First steps with React for portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { Header } from "./components/Header";
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { Projects } from './components/Projects';
+import { Cv } from './components/Cv';
+import { Contact } from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
+<div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>
+          Junior Front-end Developer
+        </h3>
+        <h1>
+          Jonathan
+        </h1>
+          <Router>
+            <Link to="/"><button className="btn btn-outline-light col-2 p-2" type="button"> Home </button></Link>
+            <Link to="/pokedex"><button className="btn btn-outline-light col-2 p-2" type="button"> Jonathan</button></Link>
+            <Link to="/pokedex"><button className="btn btn-outline-light col-2 p-2" type="button"> My Work</button></Link>
+            <Link to="/pokedex"><button className="btn btn-outline-light col-2 p-2" type="button"> Curriculum Vitae</button></Link>
+            <Link to="/pokedex"><button className="btn btn-outline-light col-2 p-2" type="button"> Connection</button></Link>
+            <Header/>
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/About" component={About} />
+              <Route path="/Projects" component={Projects} />
+              <Route path="/Cv" component={Cv} />
+              <Route path="/Contact" component={Contact} />
+            </Switch>
+          </Router>
       </header>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <>
+        </>
+            //  <li class="navIcon-container">
+            //      <a href="https://linkedin.com/in/jonathan-kalonji-0819a492/"
+            //          class="nav-icon"
+            //          target="_blank"><i class="fab fa-linkedin-in"></i>
+            //      </a>
+            //      <a href="https://github.com/Jonathanazerty/"
+            //          class="nav-icon" target="_blank"
+            //          ><i class="fab fa-github"></i>
+            //      </a>
+            //      <a class="nav-icon" 
+            //          href="mailto:j.kalonji@hotmail.com"
+            //          target="_blank"><i class="far fa-envelope"></i>
+    )
+};
+
+export {Header};
