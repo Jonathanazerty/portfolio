b415a546650b38410f52af0e0af7abf5e3e16a10
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-11-15 14:24:47 +0100

    add new project Inner peace to carousel of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import innerpeace from '../images/Inner Peace.gif';
 import pokedex from '../images/pikachu.png';
 import weather from '../images/weather-app.png';
 import cuisine from '../images/culture food.png';
@@ -15,6 +16,29 @@ const Projects = () => {
         </div>
         
         <Carousel class="carousel mb-5 mt-5">
+            <Carousel.Item interval={4000}>
+                <div className="images" class="img-responsive center-block">
+                    <img className="image" src={innerpeace} alt="loading..." class="img-fluid mb-5 mt-5" style={{width: "300px", height: "auto", borderRadius:"50%"}}/><br></br>
+                    <div className="image__overlay image__overlay--blur">
+                        <div className="image__title"> Inner Peace </div>
+                        <a href="https://inner-peace.netlify.app/" target="_blank" rel="noreferrer noopener"><p className ="image__description">
+                            Click here to view live !
+                        </p></a>
+                        <a href="https://github.com/Jonathanazerty/inner-peace"  target="_blank" rel="noreferrer noopener"><p className ="image__description">
+                            Github
+                        </p></a>
+                        <p className ="image__description">An API was used here</p>
+                        <div className="devicons">
+                            <i class="devicon-react-original-wordmark fa-3x p-3 text-warning"></i>
+                            <i class="devicon-javascript-plain fa-3x p-3 text-warning"></i>
+                            <i class="devicon-css3-plain-wordmark fa-3x p-3 text-warning"></i>
+                            <i class="devicon-bootstrap-plain-wordmark fa-3x p-3 text-warning"></i>
+                        </div>
+                    </div>
+                </div>
+                <Carousel.Caption>
+                </Carousel.Caption>
+            </Carousel.Item>
             <Carousel.Item interval={4000}>
                 <div className="images" class="img-responsive center-block">
                     <img className="image" src={pokedex} alt="loading..." class="img-fluid mb-5 mt-5" style={{width: "300px", height: "auto"}}/><br></br>
